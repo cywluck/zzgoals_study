653403f926d54f6cf31b7b5a54645953215e46c2
chenyanwu <chenyanwu@qihuiwang.com>
2018-09-24 18:14:49 +0800

    add commit

---
+++ web/0_Core/core/Q.dom.js
@@ -0,0 +1,24 @@
+Q.dom.htmlencode=function(s) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(s));
+    return div.innerHTML;
+}
+Q.dom.htmldecode=function(s) {
+    var div = document.createElement('div');
+    div.innerHTML = s;
+    return div.innerText || div.textContent;
+}
+
+Q.dom.dromhtml=function(s) {
+    var div = document.createElement('div');
+    div.innerHTML = s;
+    return div.innerText || div.textContent;
+}
+
+function removeHTMLTag(str) {
+    str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
+    str = str.replace(/[ | ]*\n/g, '\n'); //去除行尾空白
+    str = str.replace(/\n[\s| | ]*\r/g, '\n'); //去除多余空行
+    str = str.replace(/&nbsp;/ig, '');//去掉&nbsp;
+    return str;
+}
