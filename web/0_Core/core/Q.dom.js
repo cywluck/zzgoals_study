1b0027520140b5a27f604f7c3f9288bac33b2496
cywluck <cywluck@126.com>
2018-09-24 20:41:40 +0800

    edit dom.js

--- web/0_Core/core/Q.dom.js
+++ web/0_Core/core/Q.dom.js
@@ -1,21 +1,15 @@
-Q.dom.htmlencode=function(s) {
+Q.dom.htmlEncode=function(s) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(s));
     return div.innerHTML;
 }
-Q.dom.htmldecode=function(s) {
+Q.dom.htmlDecode=function(s) {
     var div = document.createElement('div');
     div.innerHTML = s;
     return div.innerText || div.textContent;
 }
 
-Q.dom.dromhtml=function(s) {
-    var div = document.createElement('div');
-    div.innerHTML = s;
-    return div.innerText || div.textContent;
-}
-
-function removeHTMLTag(str) {
+Q.dom.removeHtml=function(str) {
     str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
     str = str.replace(/[ | ]*\n/g, '\n'); //去除行尾空白
     str = str.replace(/\n[\s| | ]*\r/g, '\n'); //去除多余空行
